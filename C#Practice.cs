a268a3f46a4a68eb1a9262391b813a70c88e78d1
Yun Jang <jang.yunj7@gmail.com>
2023-01-18 23:55:40 -0500

    initial commit

---
+++ C#Practice.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter your name: ");
+string name = Console.ReadLine();
+
+Console.Write("Enter your age: ");
+string age = Console.ReadLine();
+
+Console.Write($"Hello {name}, you are {age} years old.");
